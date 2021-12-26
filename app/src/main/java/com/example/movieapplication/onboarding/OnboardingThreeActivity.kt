9c8b15caecf3389518293bbebe6d8cbcd6518f4f
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2021-12-26 14:19:29 +0700

    Onboarding - Add and Fix Manifest

---
+++ app/src/main/java/com/example/movieapplication/onboarding/OnboardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movieapplication.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapplication.R
+
+class OnboardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three)
+    }
+}
